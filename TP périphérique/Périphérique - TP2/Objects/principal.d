4aabfbc87c4c8d1d729e9d3fb9ecba54b7615846
thomaswim <39349336+thomaswim@users.noreply.github.com>
2021-09-23 17:28:23 +0200

    Add files via upload

--- TP périphérique/Périphérique - TP2/Objects/principal.d
+++ TP périphérique/Périphérique - TP2/Objects/principal.d
@@ -7,3 +7,4 @@
 .\objects\principal.o: C:\Programdata\Keil\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\cmsis_compiler.h
 .\objects\principal.o: C:\Programdata\Keil\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\cmsis_armcc.h
 .\objects\principal.o: C:\Programdata\Keil\Arm\Packs\Keil\STM32F1xx_DFP\2.3.0\Device\Include\system_stm32f10x.h
+.\objects\principal.o: ..\Mes drivers\include\Driver_GPIO.h
--- TP périphérique/Périphérique - TP2/Source/Principal.c
+++ TP périphérique/Périphérique - TP2/Source/Principal.c
@@ -1,33 +1,54 @@
 #include "stm32f10x.h"
+#include "Driver_GPIO.h"
 
-
+	
 
 
 int main ( void )
 {
+		MyGPIO_Struct_TypeDef PC13 ;
+		MyGPIO_Struct_TypeDef PA5 ;
 	
-	int marcel ;
-// GPIOC_CRL => faut le passer en 01 pour passer en Floating input PC 13 
-	GPIOC -> CRH = GPIOC->CRH &~(0xF<<20);
-	GPIOC -> CRH = GPIOC->CRH |(0x04<<20);
+		RCC->APB2ENR |= (0x01 << 2) | (0x01 << 3) | (0x01 << 4) ;
+	
+		PC13.GPIO = GPIOC ;
+		PC13.GPIO_Pin = 13 ;
+		PC13.GPIO_Conf = In_Floating ;
+	
+
+
+		PA5.GPIO = GPIOA ;
+		PA5.GPIO_Pin = 5 ;
+		PA5.GPIO_Conf = Out_Ppull ;
+	
+	MyGPIO_Init ( &PC13 );
+	MyGPIO_Init ( &PA5 );
+	
+
+
+
+	
+
+//// GPIOC_CRL => faut le passer en 01 pour passer en Floating input PC 13 
+//	GPIOC -> CRH = GPIOC->CRH &~(0xF<<20);
+//	GPIOC -> CRH = GPIOC->CRH |(0x04<<20);
+//	
+//	
+//	// LED en output push pull (GPIOA_CRL => en mode output 00) 
+//	GPIOA -> CRL = GPIOA-> CRL &~(0xF<<20);
+//	GPIOA -> CRL = GPIOA-> CRL |(0x01<<20);
 	
 	
-	// LED en output push pull (GPIOA_CRL => en mode output 00) 
-	GPIOA -> CRL = GPIOA-> CRL &~(0xF<<20);
-	GPIOA -> CRL = GPIOA-> CRL |(0x01<<20);
-	
-	marcel = MyGPIO_Read ( GPIOA ,11 ); 
 	
 	do
 {
 	
 	
 	// Quand GPIOC PC13==1 alors GPIOA PA5=1 
-	if(GPIOC -> IDR & (0x1<<13))
-		
-		GPIOA->BRR = (0x01<<5);
+	if(MyGPIO_Read(GPIOC , 13))
+		MyGPIO_Reset(GPIOA , 5 );
 	else {
-		GPIOA->BSRR = (0x01<<5);
+		MyGPIO_Set(GPIOA , 5 );
 	}
 	
 		
